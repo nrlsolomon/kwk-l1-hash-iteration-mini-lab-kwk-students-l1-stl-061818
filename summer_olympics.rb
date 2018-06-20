6fa6a5d5950ac0e9b9f093ac2e424675636eafec
nrlsolomon <nrlsolom5@gmail.com>
2018-06-20 19:14:35 +0000

    Done.

---
+++ summer_olympics.rb
@@ -0,0 +1,9 @@
+summer_olympics = {"sydney" => 2000, "athens" => 2004, "beijing" => 2008, "london" => 2012}
+
+summer_olympics.each do |place, year|
+  
+  puts "the #{place} summer olympics took place in #{year}"
+  puts""
+  
+  summer_olympics
+ end 
