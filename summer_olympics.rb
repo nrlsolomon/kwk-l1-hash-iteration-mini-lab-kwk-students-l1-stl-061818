09f13a7b1dac48a9e603f7bf68066eb4ae615111
nrlsolomon <nrlsolom5@gmail.com>
2018-06-20 19:09:18 +0000

    automatically backed up by learn

--- summer_olympics.rb
+++ summer_olympics.rb
@@ -1,5 +1,7 @@
 summer_olympics = {"sydney" => 2000, "athens" => 2004, "beijing" => 2008, "london" => 2012}
 
-summer_olympics.each do |place,year|
+summer_olympics.each do |place, year|
   
-  puts 
+  puts "the #{place} summer olympics took place in #{year}"
+  puts"
+  
